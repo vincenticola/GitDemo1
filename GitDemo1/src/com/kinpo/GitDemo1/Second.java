8586b76cb1d1eb13894fcb207ddf48ab74fae69c
Vincent <vincent_lin@kinpo.com.tw>
2012-08-13 20:01:51 +0800

    Add XML, Acitivity

---
+++ GitDemo1/src/com/kinpo/GitDemo1/Second.java
@@ -0,0 +1,14 @@
+package com.kinpo.GitDemo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+		}
+}
